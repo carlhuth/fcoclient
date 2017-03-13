f5d48be382719e5238bf84a020a4c42afa3f4ed9
Tadej Borovšak <tadej.borovsak@xlab.si>
2017-03-13 16:25:00 +0100

    Add product offer listing functionality
    FCO client is now capable of listing product offers that are
    configured for selected user.
    
    In order to get to this stage, we created base resource that all other
    resources will be derived from. This makes it possible to keep common
    functionality in single class.
    
    We as implemented actual REST client that talks to remote service.
    It can communicate with any FCO endpoint, but is currently configured
    onyl for product offers. This will change as we gradually add more
    functionality to the client.
    
    In order to make this functionality usable, we created command line
    client named fco that can be used to display offers from the comfort
    of our favourite terminal emulator.

---
+++ fcoclient/base.py
@@ -0,0 +1,153 @@
+# Copyright (c) 2017 XLAB d.o.o.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Module with base types.
+
+Classes in this module serve as a base for all other resources and clients.
+"""
+
+from requests import codes
+
+from fcoclient import exceptions
+
+
+class Resource(dict):
+    """
+    Resource is simple data container with few additional helpers.
+    """
+
+    resource_type = None
+
+    @property
+    def uuid(self):
+        """
+        Convenience accessor for resourceUUID.
+        """
+        return self["resourceUUID"]
+
+    @property
+    def name(self):
+        """
+        Convenience accessor for resourceName.
+        """
+        return self["resourceName"]
+
+    @staticmethod
+    def normalize(data):
+        """
+        Replace shorthands 'uuid' and 'name' with full names. All other key
+        value pairs are left intact. This is another convenience for writing
+        filter experessions using name and uuid keys.
+        """
+        reps = {
+            "name": "resourceName",
+            "uuid": "resourceUUID",
+        }
+        return {reps.get(k, k): v for k, v in data.items()}
+
+    def __str__(self):
+        return "{}({})".format(self.resource_type, self.uuid)
+
+
+class BaseClient(object):
+    """
+    Base client that contains common functionality for all FCO clients.
+    """
+
+    _prefix = "resources"
+    klass = None
+
+    @staticmethod
+    def _get_filter(conditions):
+        """
+        Construct a filter expression for FCO API.
+        """
+        conds = [{"field": k, "value": [v], "condition": "IS_EQUAL_TO"}
+                 for k, v in conditions.items()]
+        return {"filterConditions": conds}
+
+    @staticmethod
+    def _get_query_limit(no_items):
+        """
+        Construct query limit expression for FCO API.
+        """
+        return {
+            "from": 0,
+            "to": no_items,
+            "maxRecords": no_items,
+            "loadChildren": True,
+            "orderBy": [{
+                "aggregationFunction": None,
+                "fieldName": "resourceName",
+                "sortOrder": "ASC",
+            }],
+        }
+
+    def __init__(self, client):
+        assert self.klass is not None, \
+            "'klass' not set for '{}'.".format(self.__class__)
+        assert self.klass.resource_type is not None, \
+            "'resource_type' not set for '{}'.".format(self.klass)
+        self.client = client
+        self.endpoint = "{}/{}".format(self._prefix, self.klass.resource_type)
+
+    def list(self, no_items=200, **conditions):
+        """
+        List items that match conditions.
+
+        Use this method to retrieve list of resources from FCO API that match
+        all of the conditions. This method does not raise any exception when
+        no resource matches conditions specified.
+
+        Args:
+            no_items (int): Maximum number of items to return.
+            **conditions: Conditions that are used to filter the resources.
+
+        Returns:
+            List of resources that match conditions.
+        """
+        endpoint = self.endpoint + "/list"
+        data = {}
+        conditions = Resource.normalize(conditions)
+        data["searchFilter"] = self._get_filter(conditions)
+        data["queryLimit"] = self._get_query_limit(no_items)
+        resources = self.client.post(endpoint, data, codes.ok)["list"]
+        return [self.klass(**r) for r in resources]
+
+    def get(self, **conditions):
+        """
+        Retrieve single resource that matches conditions.
+
+        Use this method to retrieve single resource from FCO API that match
+        all of the conditions. If the conditions do not describe a resource
+        uniquely, exception is raised.
+
+        Args:
+            **conditions: Conditions that are used to filter the resources.
+
+        Returns:
+            List of resources that match conditions.
+
+        Raises:
+            NonUniqueResourceError: If more than one resource matches.
+            NoSuchResourceError: If no resource matches conditions.
+        """
+        data = self.list(**conditions)
+        if len(data) > 1:
+            raise exceptions.NonUniqueResourceError(conditions)
+        elif len(data) < 1:
+            raise exceptions.NoSuchResourceError(conditions)
+        return data[0]
---
+++ fcoclient/cli.py
@@ -0,0 +1,195 @@
+# Copyright (c) 2017 XLAB d.o.o.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import print_function
+
+import argparse
+import inspect
+import json
+import logging
+import sys
+
+from fcoclient.client import Client
+
+# Logging
+fmt_stream = logging.Formatter("[%(levelname)s] - %(message)s")
+handler_stream = logging.StreamHandler()
+handler_stream.setFormatter(fmt_stream)
+handler_stream.setLevel(logging.INFO)
+
+fmt_file = logging.Formatter(
+    "%(asctime)s %(name)s:%(lineno)s [%(levelname)s] - %(message)s"
+)
+handler_file = logging.FileHandler(".fco.log")
+handler_file.setFormatter(fmt_file)
+handler_file.setLevel(logging.DEBUG)
+
+logger = logging.getLogger("fcoclient")
+logger.addHandler(handler_stream)
+logger.addHandler(handler_file)
+logger.setLevel(logging.DEBUG)
+
+
+def fail(msg, *args):
+    logger.error(msg.format(*args))
+    sys.exit(1)
+
+
+def display(item):
+    print(json.dumps(item, indent=2))
+
+
+class ArgParser(argparse.ArgumentParser):
+    """
+    Argument parser that displays help on error
+    """
+
+    def error(self, message):
+        sys.stderr.write("error: {}\n".format(message))
+        self.print_help()
+        sys.exit(2)
+
+    def add_subparsers(self):
+        # Workaround for http://bugs.python.org/issue9253
+        subparsers = super(ArgParser, self).add_subparsers()
+        subparsers.required = True
+        subparsers.dest = "command"
+        return subparsers
+
+
+class Config(dict):
+
+    valid_keys = {"url", "username", "customer", "password", "verify"}
+
+    def __init__(self, data={}):
+        invalid = [k for k in data.keys() if k not in self.valid_keys]
+        if len(invalid) != 0:
+            fail("Invalid settings key(s) found: {}", ",".join(invalid))
+
+        # TODO: Fix this as soon as possible!!!
+        self["verify"] = False
+        self.update(data)
+
+    def save(self, path):
+        with open(path, "w") as f:
+            json.dump(self, f, indent=2)
+
+    # Construction helper
+    @staticmethod
+    def load_from_file(path, fail_on_missing=True):
+        try:
+            with open(path) as f:
+                data = json.load(f)
+        except IOError:
+            if fail_on_missing:
+                fail("File {} is missing", path)
+            data = {}  # Missing file simply means no settings are present yet
+        except:
+            fail("File {} is not valid JSON", path)
+        return Config(data)
+
+
+class Command(object):
+
+    @staticmethod
+    def add_subparser(subparsers):
+        raise NotImplementedError("Command is an abstract class")
+
+    def __init__(self, config_path):
+        self.client = Client(**Config.load_from_file(config_path))
+
+
+class Configure(Command):
+
+    @staticmethod
+    def add_subparser(subparsers):
+        parser = subparsers.add_parser("configure", help="Configure client")
+        parser.add_argument("url", help="FCO URL")
+        parser.add_argument("username", help="FCO username")
+        parser.add_argument("customer", help="FCO customer")
+        parser.add_argument("password", help="FCO password")
+        return parser
+
+    def __init__(self, config_path):
+        pass  # Prevent client from being constructed
+
+    def configure(self, args):
+        logger.info("Configuring client")
+        config = Config.load_from_file(args.config, fail_on_missing=False)
+        config["url"] = args.url
+        config["username"] = args.username
+        config["customer"] = args.customer
+        config["password"] = args.password
+        config.save(args.config)
+        logger.info("Client configured")
+
+
+class Offer(Command):
+
+    @staticmethod
+    def add_subparser(subparsers):
+        parser = subparsers.add_parser("offer", help="Inspect product offers")
+        subs = parser.add_subparsers()
+
+        sub = subs.add_parser("list", help="List product offers")
+        sub.add_argument("-n", "--no-items", type=int, default=100,
+                         help="Numbr of offers to display")
+        sub.add_argument("-t", "--type",
+                         help="Only display offers for associated type")
+
+        sub = subs.add_parser("get", help="Get details about selected offer")
+        sub.add_argument("uuid", help="Offer UUID")
+
+        return parser
+
+    def list(self, args):
+        logger.info("Listing product offers")
+        conditions = {}
+        if args.type is not None:
+            conditions["productAssociatedType"] = args.type
+        for po in self.client.product_offer.list(args.no_items, **conditions):
+            print("{}: {} ({})".format(po["productAssociatedType"], po.name,
+                                       po.uuid))
+        logger.info("Offers listed")
+
+    def get(self, args):
+        logger.info("Getting product offer details")
+        display(self.client.product_offer.get(uuid=args.uuid))
+        logger.info("Product offer details retrieved")
+
+
+def create_parser():
+    def is_command(item):
+        return (inspect.isclass(item) and item != Command and
+                issubclass(item, Command))
+
+    parser = ArgParser(description="DICE Deployment Service CLI",
+                       formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--config", help="Configuration file to use",
+                        default=".fco.conf")
+    subparsers = parser.add_subparsers()
+
+    commands = inspect.getmembers(sys.modules[__name__], is_command)
+    for _, cls in commands:
+        sub = cls.add_subparser(subparsers)
+        sub.set_defaults(cls=cls)
+
+    return parser
+
+
+def main():
+    parser = create_parser()
+    args = parser.parse_args()
+    getattr(args.cls(args.config), args.command)(args)
