a60b46347341dbf28223ac940bfa7fd02ccdf1f0
Tadej Borovšak <tadej.borovsak@xlab.si>
2017-03-15 23:51:05 +0100

    Add server creation support
    Client is now capable of creating new server instances. Starting and
    stopping is not yet supported.

--- fcoclient/commands/server.py
+++ fcoclient/commands/server.py
@@ -27,9 +27,37 @@ class ServerCmd(Command):
 
         Command.create_get_parser(subs, "server")
         Command.create_list_parser(subs, "servers")
+        Command.create_skeleton_parser(subs, "server")
+        Command.create_new_parser(subs, "server")
+        Command.create_delete_parser(subs, "server")
 
         return parser
 
     @property
     def resource_client(self):
         return self.client.server
+
+    def create(self, args):
+        self.logger.info("Creating new server")
+        skeleton = json.load(args.skeleton)
+        job = self.client.server.create(skeleton)
+        if args.wait:
+            self.logger.info("Waiting for server creation to terminate")
+            job = self.client.job.wait(job)
+            self.display(job)
+            self.logger.info("Server creation terminated")
+        else:
+            self.display(job)
+            self.logger.info("Server creation scheduled")
+
+    def delete(self, args):
+        self.logger.info("Deleting server")
+        job = self.client.server.delete(args.uuid, cascade=args.cascade)
+        if args.wait:
+            self.logger.info("Waiting for server deletion to terminate")
+            job = self.client.job.wait(job)
+            self.display(job)
+            self.logger.info("Server deletion terminated")
+        else:
+            self.display(job)
+            self.logger.info("Server deletion scheduled")
--- fcoclient/resources/server.py
+++ fcoclient/resources/server.py
@@ -30,6 +30,31 @@ class Server(Resource):
 
     resource_type = ResourceType.server
 
+    @staticmethod
+    def skeleton():
+        return Server("{SERVER PRODUCT OFFER UUID}", "{SERVER NAME}",
+                      "{VDC UUID}")
+
+    def __init__(self, productOfferUUID, resourceName, vdcUUID, **rest):
+        """
+        Create new server resource.
+
+        Detailed description of available fields can be found at server_ docs.
+
+        .. _server: http://docs.flexiant.com/display/DOCS/REST+Server
+
+        Args:
+            productOfferUUID: UUID of the offer
+            resourceName: Name of new server
+            vdcUUID: UUID of the VDC where server will reside
+            **rest: Additional skeleton fields
+        """
+        super(Server, self).__init__({
+            "productOfferUUID": productOfferUUID,
+            "resourceName": resourceName,
+            "vdcUUID": vdcUUID,
+        }, **rest)
+
 
 class ServerClient(BaseClient):
     """
@@ -37,3 +62,19 @@ class ServerClient(BaseClient):
     """
 
     klass = Server
+
+    def create(self, skeleton):
+        """
+        Create new server.
+
+        Server creation is asynchronous operation and needs to be tracked
+        using job object that is returned.
+
+        Args:
+            skeleton (:obj:`Server`): Server skeleton.
+
+        Returns:
+           :obj:`Job`: New job, describing creation progress.
+        """
+        data = {"skeletonServer": skeleton}
+        return Job(self.client.post(self.endpoint, data, codes.accepted))
